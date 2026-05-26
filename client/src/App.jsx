004ade77b03e516c73a820e5c1a7481a5f196bd8
kkarthik2k26 <iamkarthikk06@gmail.com>
2026-05-27 01:37:53 +0530

    fixing lint issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  let bugga = 'unused variable here';
+  //let bugga = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
